a109b64727c2f1547dfbc6da52f628882cd49d7e
Daniel Koo <daniel.koo@iag.com.au>
2022-04-21 09:53:47 +1000

    python 3.8.12 on asdf fails to compile on the IAG MBP 2019 laptop, but 3.8.13 is ok

--- bootstrap/step4.sh
+++ bootstrap/step4.sh
@@ -6,6 +6,6 @@ echo "setting up python"
 if ! asdf plugin list | grep '^python$' > /dev/null
 then
   asdf plugin add python
-  asdf install python 3.8.12 # the mac-dev-playbook wants 3.8, but could test 3.9+ in the future
-  asdf global python 3.8.12
+  asdf install python 3.8.13 # the mac-dev-playbook wants 3.8, but could test 3.9+ in the future
+  asdf global python 3.8.13
 fi
