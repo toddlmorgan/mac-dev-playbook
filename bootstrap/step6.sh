5c8efad1688001442b7b9dd19b97f414117b7a44
Daniel Koo <1916351+danielkoo@users.noreply.github.com>
2022-05-04 22:49:33 +1000

    added a step to install dockutil

--- bootstrap/step6.sh
+++ bootstrap/step6.sh
@@ -3,11 +3,12 @@
 # source this so we can start running ansible, straight after installing it via asdf
 # note this is being done before `set -u` as an undefined $ASDF_DIR will cause the
 # script to fail
-source $(brew --prefix asdf)/libexec/asdf.sh
 
 set -euo pipefail
-echo "installing mac-dev-playbook requirements"
+echo "installing dockutil v3 separately as homebrew does not support it yet"
+echo "see https://github.com/kcrawford/dockutil/issues/127"
 
-
-ansible-galaxy install -r requirements.yml
-ansible-playbook --skip-tags "asdf-install,dock" main.yml --ask-become-pass
+DLURL=$(curl --silent "https://api.github.com/repos/kcrawford/dockutil/releases/latest" | jq -r .assets[].browser_download_url | grep pkg)
+curl -sL ${DLURL} -o /tmp/dockutil.pkg
+sudo installer -pkg "/tmp/dockutil.pkg" -target /
+rm /tmp/dockutil.pkg
---
+++ bootstrap/step7.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# source this so we can start running ansible, straight after installing it via asdf
+# note this is being done before `set -u` as an undefined $ASDF_DIR will cause the
+# script to fail
+source $(brew --prefix asdf)/libexec/asdf.sh
+
+set -euo pipefail
+echo "installing mac-dev-playbook requirements"
+
+
+ansible-galaxy install -r requirements.yml
+ansible-playbook --skip-tags "asdf-install" main.yml --ask-become-pass
