0bbd48a6e9ccdcae346b031b61892892b6cbbd0d
Daniel Koo <1916351+danielkoo@users.noreply.github.com>
2022-05-06 00:27:38 +1000

    added additional steps to get golang working on an iag laptop

---
+++ iag/todo.sh
@@ -0,0 +1,43 @@
+
+# generate ssh key so I can clone from chuck
+# brew install cntlm
+# copy cntlm.conf from OneDrive
+export http_proxy=http://localhost:3128
+export https_proxy=http://localhost:3128
+export HTTP_PROXY=http://localhost:3128
+export HTTPS_PROXY=http://localhost:3128
+git config --global http.proxy http://localhost:3128
+git config --global https.proxy http://localhost:3128
+
+
+asdf plugin add vault
+asdf install vault 0.11.6
+asdf global vault 0.11.6
+asdf plugin add terraform
+asdf install terraform 0.11.3
+asdf global terraform 0.11.3
+
+asdf plugin add golangci-lint
+asdf install golangci-lint 1.43.0
+asdf global golangci-lint 1.43.0
+
+asdf plugin add snyk
+asdf install snyk latest
+asdf global snyk latest
+
+# copy snyk api to ~/.config/configstore/snyk.json
+
+# copy ca-certs.pem from OneDrive to /etc/ssl/certs/ca-certificates.crt
+
+####################
+# to build devlabsctl on the mac
+asdf install golang 1.16
+asdf global golang 1.16
+go get -u github.com/go-bindata/go-bindata/...
+go install github.com/jstemmer/go-junit-report@latest
+go get  github.com/optiopay/klar
+
+# download klar
+https://github.com/optiopay/klar/releases
+
+####################
